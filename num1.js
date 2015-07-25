5ab46b9e431597e4bd79a314da70e43b42485d0a
Tomo Sindberg <tsindberg@gmail.com>
2015-07-24 20:49:33 -0700

    All Finished

--- num1.js
+++ num1.js
@@ -1,76 +1,75 @@
-// var _ = require('lodash');
-// _.mixin(require('lodash-toolbox'));
-// var lodash = require('lodash');
-
-//require(['lodash'], function(_) {});
+//var lodash = require('lodash');
 //var _ = require('lodash');
-// var array = require('lodash/array');
-// var chunk = require('lodash/array/chunk');
-
-
-var sum = 0,
-  numProgrammers2 = 0,
-  avgAwesome2 = 0,
-  j = 0;
-
 
 var people =[
-{
-name: 'Bob',
-occupation: 'programmer',
-awesomeIndex: 7,
-},
+  {
+  name: 'Bob',
+  occupation: 'programmer',
+  awesomeIndex: 7,
+  },
 
-{
-name: 'Alice',
-occupation: 'programmer',
-awesomeIndex: 8,
-},
+  {
+  name: 'Alice',
+  occupation: 'programmer',
+  awesomeIndex: 8,
+  },
 
-{
-name: 'Zaphod',
-occupation: 'President of the Galaxy',
-},
+  {
+  name: 'Zaphod',
+  occupation: 'President of the Galaxy',
+  },
 
-{
-name: 'Alice',
-occupation: 'programmer',
-awesomeIndex: 9,
-},
+  {
+  name: 'Alice',
+  occupation: 'programmer',
+  awesomeIndex: 7,
+  },
 
-{
-name: 'Julie',
-occupation: 'x-programmer',
-awesomeIndex: 3,
-}
+  {
+  name: 'Julie',
+  occupation: 'x-programmer',
+  awesomeIndex: 3,
+  }
 ];
 
-console.log(avgAwesome());
+function staffInfo() {
+  var text = [];
+  var i = 0;
+  _.forEach(people, function(n) {
+    text += "<tr><td>" + "Name:  " + n.name + "<td>" +
+    "Occupation:  " + n.occupation + "<td>" +
+    "Awesome Index:  " + n.awesomeIndex + "</tr>";
+    i++;
+  });
+  return text;
+}
 
-function avgAwesome() {
-  var total = 0;
+function average() {
+  var sum = 0;
   var numProgrammers = 0;
-  for (i=0; i<people.length; i++){
-    if (people[i].occupation === 'programmer') {
-      total += people[i].awesomeIndex;
+
+  _.forEach(people, function(n) {   //could run (n,i)
+    if(n.occupation == 'programmer') {
+      sum += n.awesomeIndex;
       numProgrammers += 1;
     }
-  }
-  return total/numProgrammers;
+  });
+  return (sum/numProgrammers).toFixed(1);
 }
-//-------------------------
-//var test =
-// _.forEach(people, function(){
-//   if (people[j].occupation == 'programmer') {
-//     sum += _.add(people[j].awesomeIndex);
-//     numProgrammers2 += 1;
-//   }
-//   j++;
-//   avgAwesome2 = sum/numProgrammers2;
-// });
 
-// console.log(avgAwesome2);
-//console.log(test);
+$("#staff").click (function(){
+  console.log("clicked staff button");
+  $("#name").html(staffInfo());
+});
+
+$("#AvgAwesomeButton").click (function(){
+  console.log("clicked Awesome button");
+
+  $("#awesome-index").text("Average Awesomeness:  " + average());
+});
+
+
+
 
 
 
