dc057a5ab4d428e0b66b3fdb84407ce8a0311e39
Tomo Sindberg <tsindberg@gmail.com>
2015-08-19 20:40:31 -0700

    Finished

--- num1.js
+++ num1.js
@@ -1,5 +1,5 @@
-//var lodash = require('lodash');
-//var _ = require('lodash');
+var lodash = require('lodash');  //comment out to run with HTML
+var _ = require('lodash');    //same as above
 
 var people =[
   {
@@ -32,45 +32,62 @@ var people =[
   }
 ];
 
+// staffInfo();
+// average();
+console.log(average());
 function staffInfo() {
   var text = [];
-  var i = 0;
+  //var i = 0;
   _.forEach(people, function(n) {
     text += "<tr><td>" + "Name:  " + n.name + "<td>" +
     "Occupation:  " + n.occupation + "<td>" +
     "Awesome Index:  " + n.awesomeIndex + "</tr>";
-    i++;
+    //i++;
   });
+  // console.log(text);
   return text;
 }
 
 function average() {
   var sum = 0;
-  var numProgrammers = 0;
+  var aweArray = 0;
 
-  _.forEach(people, function(n) {   //could run (n,i)
-    if(n.occupation == 'programmer') {
-      sum += n.awesomeIndex;
-      numProgrammers += 1;
-    }
+  aweArray = _.pluck(_.filter(people, {'occupation':'programmer'}),'awesomeIndex');
+  sum = _.reduce(aweArray, function(total, i){
+    return total + i;
   });
-  return (sum/numProgrammers).toFixed(1);
+  // var aweAvg = sum/_.size(aweArray);
+// console.log(aweArray);
+// console.log(sum);
+
+  return _.round((sum/_.size(aweArray)),1);
+  // var aweAvg = _.round(z,1);
+// var x = _.filter(people, {'occupation':'programmer'});//,'awesomeIndex');
+
+// console.log(z);
+// console.log(aweAvg);
+  // _.forEach(people, function(n) {   //could run (n,i)  people[i]
+  //   if(n.occupation == 'programmer') {  //_.filter, pluck, map and reduce
+  //     sum += n.awesomeIndex;          //remove forEach and If
+  //     numProgrammers += 1;
+  //   }
+  // });
+  // console.log(sum/numProgrammers);
+  // return (sum/numProgrammers).toFixed(1);
 }
 
-$("#staff").click (function(){
-  console.log("clicked staff button");
-  $("#name").html(staffInfo());
-});
+function timesThree(n) {
+  return n * 3;
+}
 
-$("#AvgAwesomeButton").click (function(){
-  console.log("clicked Awesome button");
+var x = _.map([1, 2], timesThree);
+console.log(x);
+// $("#staff").click (function(){
+//   console.log("clicked staff button");
+//   $("#name").html(staffInfo());
+// });
 
-  $("#awesome-index").text("Average Awesomeness:  " + average());
-});
-
-
-
-
-
-
-
+// $("#AvgAwesomeButton").click (function(){
+//   console.log("clicked Awesome button");
+//   $("#awesome-index").text("Average Awesomeness:  " + average());
+// });
